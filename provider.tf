47e7d76eefcd86bf27eb2b37f296e4d0e2231f70
NaveenRayapati <79656203+NaveenRayapati@users.noreply.github.com>
2026-05-08 10:04:28 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0" # Good practice to lock version
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0" # Uses the latest 5.x version
+    }
+  }
+}
+
+provider "google" {
+  project = "cloudside-academy"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
