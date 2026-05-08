258b8fa287ec78b61b16e9642ef82b8fd0aa593a
NaveenRayapati <79656203+NaveenRayapati@users.noreply.github.com>
2026-05-08 11:33:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,7 @@
 terraform {
-  required_version = ">= 1.5.0" # Good practice to lock version
-
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0" # Uses the latest 5.x version
+      source = "hashicorp/google"
     }
   }
-}
-
-provider "google" {
-  project = "cloudside-academy"
-  region  = "us-central1
 }
